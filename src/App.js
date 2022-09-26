965f9e911d641d26aeb297e06b25d575586ad2e9
malickbax <malickbax@gmail.com>
2022-09-26 09:15:41 -0400

    Inital REACT files

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Nav from './components/Nav';
+import About from './components/About';
+
+function App() {
+
+  return (
+    <div>
+
+      <main>
+        <Nav />
+        <About />        
+      </main>
+
+    </div>
+  );
+}
+
+export default App;

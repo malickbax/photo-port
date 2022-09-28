8b98cb70a3dc705c6c40f62e3576189bc84ee007
malickbax <malickbax@gmail.com>
2022-09-28 16:23:23 -0400

    Adjusted package.json for deployment on GitHub Pages

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -2,7 +2,6 @@ import React from 'react';
 
 const Modal = ({ onClose, currentPhoto }) => {
   const { name, description, category, index } = currentPhoto;
-
   return (
     <div className="modalBackdrop">
       <div className="modalContainer">
